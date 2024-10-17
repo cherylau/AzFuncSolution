e945dd2d7e349bfdcb699c488797ac4fb006be3a
Cheryl Lau <clau136@my.bcit.ca>
2024-10-17 11:24:03 -0700

    First commit

---
+++ StudentsSPA/Constants.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsSPA
+{
+  public class Constants
+  {
+    // public const string BASE_URL = "https://bogus.azurewebsites.net/api/";
+    public const string BASE_URL = "/api/"
+    }
+}
